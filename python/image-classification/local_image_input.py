3a6d568e42395941ea7564de2d53814cd93398d5
maheshpatil2021 <mahesh@tiyaro.ai>
2022-05-12 12:00:59 -0700

    Code Samples - The beginning...

---
+++ python/image-classification/local_image_input.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+Sample code to run image classification with local image as input
+"""
+
+import requests
+import os
+import sys
+import base64
+
+
+def imageToBase64(srcPath):
+    with open(srcPath, 'rb') as image:
+        b64Img = base64.b64encode(image.read()).decode('utf-8')
+    return b64Img
+
+
+def infer():
+    # Get the API key for invoking Tiyaro API
+    apiKey = os.getenv("TIYARO_API_KEY")
+    if apiKey is None:
+        print("Please set TIYARO_API_KEY environment variable. You can generate your API key from here - https://console.tiyaro.ai/apikeys")
+        sys.exit(1)
+
+    # API endpoint
+    url = "https://api.tiyaro.ai/v1/ent/google/5/imagenet/resnet_v1_101/classification"
+
+    # Convert binary image to base64
+    imgPath = "../../testdata/object-detect-1.jpg"
+    b64Img = imageToBase64(imgPath)
+
+    payload = {"imageRef": {"Bytes": b64Img}}
+    headers = {
+        "accept": "*/*",
+        "content-type": "application/json",
+        "authorization": f"Bearer {apiKey}"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    # Check for errors
+    response.raise_for_status()
+
+    # Inference response
+    print(response.text)
+
+
+if __name__ == "__main__":
+    infer()
