21ed640b2ddaa105ca6333636d09c4197d347e68
maheshpatil2021 <mahesh@tiyaro.ai>
2022-05-12 13:22:53 -0700

    Python sample for Object Detection
    Input formats added
    - URL
    - Local image

---
+++ python/image-object-detection/url_input.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""
+Sample code to run image classification with image url as input
+"""
+
+import requests
+import os
+import sys
+
+
+def infer():
+    # Get the API key for invoking Tiyaro API
+    apiKey = os.getenv("TIYARO_API_KEY")
+    if apiKey is None:
+        print("Please set TIYARO_API_KEY environment variable. You can generate your API key from here - https://console.tiyaro.ai/apikeys")
+        sys.exit(1)
+
+    # API endpoint
+    url = "https://api.tiyaro.ai/v1/ent/torchserve/1/maskrcnn_resnet50_fpn"
+
+    # Input image
+    imgURL = "https://public-model-demo.s3.us-west-2.amazonaws.com/object-detect-1.jpg"
+
+    payload = {"imageRef": {"URL": imgURL}}
+    headers = {
+        "accept": "*/*",
+        "content-type": "application/json",
+        "authorization": f"Bearer {apiKey}"
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    # Check for errors
+    response.raise_for_status()
+
+    # Inference response
+    print(response.text)
+
+
+if __name__ == "__main__":
+    infer()
